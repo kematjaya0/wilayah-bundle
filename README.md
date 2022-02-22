7d93b694fff304a155c5467e8ed645023308bef5
Nur Hidayatullah <kematjaya0@gmail.com>
2022-02-22 10:38:33 +0700

    console insert data

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # wilayah-bundle
 Symfony bundle untuk database wilayah di indonesia
+1. instalasi
+```
+composer require kematjaya/wilayah-bundle
+```
+2. update bundles.php
+```
+// config/bundles.php
+...
+Kematjaya\WilayahBundle\WilayahBundle::class => ['all' => true]
+...
+```
+3. update database schema
+```
+php bin/console doctrine:schema:update --force
+```
+4. insert data
+```
+// all data
+php bin/console wilayah:insert
+
+// provinsi saja
+php bin/console wilayah:insert --data=provinsi
+
+// provinsi dan kabupaten
+php bin/console wilayah:insert --data=provinsi --data=kabupaten
+```
---
+++ src/Console/DataConsole.php
@@ -0,0 +1,200 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace Kematjaya\WilayahBundle\Console;
+
+use Kematjaya\WilayahBundle\Entity\Provinsi;
+use Kematjaya\WilayahBundle\Entity\Kabupaten;
+use Kematjaya\WilayahBundle\Entity\Kecamatan;
+use Kematjaya\WilayahBundle\Entity\Desa;
+use Kematjaya\WilayahBundle\SourceReader\VillageSourceReaderInterface;
+use Kematjaya\WilayahBundle\SourceReader\DistrictSourceReaderInterface;
+use Kematjaya\WilayahBundle\SourceReader\RegionSourceReaderInterface;
+use Kematjaya\WilayahBundle\SourceReader\ProvinceSourceReaderInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+/**
+ * Description of DataConsole
+ *
+ * @author guest
+ */
+class DataConsole extends Command 
+{
+    protected static $defaultName = 'wilayah:insert';
+    
+    private $data = ['provinsi', 'kabupaten', 'kecamatan', 'desa'];
+    
+    private $configs = [];
+    
+    /**
+     * 
+     * @var ProvinceSourceReaderInterface
+     */
+    private $provinceSourceReader;
+    
+    /**
+     * 
+     * @var RegionSourceReaderInterface
+     */
+    private $regionSourceReader;
+    
+    /**
+     * 
+     * @var DistrictSourceReaderInterface
+     */
+    private $districtSourceReader;
+    
+    /**
+     * 
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    
+    /**
+     * 
+     * @var VillageSourceReaderInterface
+     */
+    private $villageSourceReader;
+    
+    public function __construct(ParameterBagInterface $bag, EntityManagerInterface $entityManager, VillageSourceReaderInterface $villageSourceReader, DistrictSourceReaderInterface $districtSourceReader, RegionSourceReaderInterface $regionSourceReader, ProvinceSourceReaderInterface $provinceSourceReader, mixed $name = null) 
+    {
+        $configs = $bag->get('wilayah');
+        $this->configs = $configs['filter'];
+        $this->entityManager = $entityManager;
+        $this->villageSourceReader = $villageSourceReader;
+        $this->districtSourceReader = $districtSourceReader;
+        $this->provinceSourceReader = $provinceSourceReader;
+        $this->regionSourceReader = $regionSourceReader;
+        parent::__construct($name);
+    }
+    
+    protected function configure()
+    {
+        $this
+            ->addOption(
+                    'data', 
+                    null, 
+                    InputOption::VALUE_IS_ARRAY|InputOption::VALUE_OPTIONAL,
+                    'Which data will you insert ?',
+                    $this->data
+                );
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        try {
+            $data = array_map(function ($row) {
+                if (!in_array($row, $this->data)) {
+                    throw new \Exception(
+                        sprintf("data '%s' not available from (%s)'", $row, implode(", ", $this->data))
+                    );
+                }
+
+                return $row;
+            }, $input->getOption('data', []));
+            
+            $provinsis = $this->provinceSourceReader->findAll(
+                $this->configs['provinsi']
+            );
+            foreach ($provinsis as $prov) {
+                
+                $io->info(
+                    sprintf("processing: '%s'", strtoupper($prov['nama']))
+                );
+                
+                $object = $this->entityManager->getRepository(Provinsi::class)->findOneBy([
+                    'code' => $prov['id']
+                ]); 
+                if (null === $object) {
+                    $object = new Provinsi();
+                }
+
+                $object->setCode($prov['id'])
+                        ->setName(strtoupper($prov['nama']));
+
+                $this->entityManager->persist($object);
+                if (!in_array('kabupaten', $data)) {
+                    continue;
+                }
+
+                $kabupatens = $this->regionSourceReader->filterByProvinceId($prov['id'], $this->configs['kabupaten']);
+                foreach ($kabupatens as $kabupaten) {
+                    $kabObject = $this->entityManager->getRepository(Kabupaten::class)->findOneBy([
+                        'code' => $kabupaten['id'], 'provinsi' => $object
+                    ]); 
+                    if (null === $kabObject) {
+                        $kabObject = new Kabupaten();
+                        $kabObject->setProvinsi($object)
+                            ->setCode($kabupaten['id']);
+                    }
+                    $kabObject
+                            ->setName(strtoupper($kabupaten['nama']));
+
+                    $this->entityManager->persist($kabObject);
+                    if (!in_array('kecamatan', $data)) {
+                        continue;
+                    }
+
+                    $kecamatans = $this->districtSourceReader->filterByRegionId($kabupaten['id'], $this->configs['kecamatan']);
+                    foreach ($kecamatans as $kecamatan) {
+                        $kecObject = $this->entityManager->getRepository(Kecamatan::class)->findOneBy([
+                            'code' => $kecamatan['id'], 'kabupaten' => $kabObject
+                        ]); 
+                        if (null === $kecObject) {
+                            $kecObject = new Kecamatan();
+                            $kecObject->setKabupaten($kabObject)
+                                ->setCode($kecamatan['id']);
+                        }
+
+                        $kecObject
+                                ->setName(strtoupper($kecamatan['nama']));
+
+                        $this->entityManager->persist($kecObject);
+                        if (!in_array('desa', $data)) {
+                            continue;
+                        }
+
+                        $villages = $this->villageSourceReader->filterByDistrictId($kecObject->getCode());
+                        foreach ($villages as $village) {
+                            $desaObject = $this->entityManager->getRepository(Desa::class)->findOneBy([
+                                'code' => $kecamatan['id'], 'kecamatan' => $kecObject
+                            ]); 
+                            if (null === $desaObject) {
+                                $desaObject = new Desa();
+                                $desaObject->setKecamatan($kecObject)
+                                    ->setCode($village['id']);
+                            }
+
+                            $desaObject
+                                    ->setName(strtoupper($village['nama']));
+
+                            $this->entityManager->persist($desaObject);
+                        }
+                    }
+                }
+            }
+
+            $this->entityManager->flush();
+            
+        } catch (\Exception $ex) {
+            $io->error($ex->getMessage());
+            
+            return self::FAILURE;
+        }
+        
+        $io->info("success");
+        
+        return self::SUCCESS;
+    }
+}
